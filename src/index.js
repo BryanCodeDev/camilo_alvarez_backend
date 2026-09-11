bbfb0b0d6f58bf43421103ecbd7fd6e3d6c9f90e
BryanCodeDev <tiagotroller021019@gmail.com>
2026-09-11 16:40:39 -0500

    feat: Actualizacion de base de datos

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import helmet from 'helmet'
 import cookieParser from 'cookie-parser'
 import rateLimit from 'express-rate-limit'
 import { connectDB, ensureDatabase } from './config/database.js'
+import { runMigrations } from './utils/migrate.js'
 import authRoutes from './routes/auth.js'
 import productRoutes from './routes/products.js'
 import categoryRoutes from './routes/categories.js'
@@ -64,6 +65,7 @@ app.use(errorHandler)
 async function start() {
   try {
     await ensureDatabase()
+    await runMigrations()
     await connectDB()
     app.listen(PORT, () => {
       console.log(`🚀 Server running on port ${PORT}`)
